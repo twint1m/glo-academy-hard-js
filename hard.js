db0152d1efcfbe52cb1ef92869360cd06459f18b
twint1m <twint1m.contact@gmail.com>
2023-05-08 13:32:39 +0300

    hard lesson done

---
+++ hard.js
@@ -0,0 +1,9 @@
+let num = 266219;
+let digits = num.toString().split('');
+let product = 1;
+for (let i = 0; i < digits.length; i++) {
+  product *= digits[i];
+}
+product = product * product * product;
+let resultString = product.toString();
+console.log(resultString.substring(0, 2));
