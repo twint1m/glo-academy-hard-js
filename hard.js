48e491b1ad11738a05b5cfe9a85fc25fee2454d5
twint1m <twint1m.contact@gmail.com>
2023-05-09 13:23:30 +0300

    third hard lesson done

--- hard.js
+++ hard.js
@@ -1,10 +1,36 @@
-// let num = 266219;
-// let digits = num.toString().split('');
-// let product = 1;
-// for (let i = 0; i < digits.length; i++) {
-//   product *= digits[i];
-// }
-// product = product * product * product;
-// let resultString = product.toString();
-// console.log(resultString.substring(0, 2));
-console.log('test');
+'use strict';
+let lang = 'en';
+let dayOfTheWeekRu = 'понедельник, вторник, среда, четверг, пятница, суббота, воскресенье';
+let dayOfTheWeekEn =  'monday, tuesday, wendnesday, thursday, friday, saturday, sunday';
+
+// if конструкция
+if (lang=='ru') {
+  console.log(dayOfTheWeekRu);
+} else if (lang=='en') {
+  console.log(dayOfTheWeekEn);
+}
+
+// конструкция switch case
+switch(lang) {
+  case 'ru':
+    console.log(dayOfTheWeekRu);
+    break
+  case 'en':
+    console.log(dayOfTheWeekEn);
+    break
+}
+
+//  многомерный массив
+const days = {
+  ru: ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+  en: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+};
+console.log(days[lang]);
+
+
+
+
+// тернарные операторы
+let namePerson = 'Александр';
+namePerson=='Артем' ? console.log('директор'):
+namePerson=='Александр' ? console.log('преподаватель') : console.log('студент');
