6c72a7384ec03a09c53152e431ea4e21d3345328
twint1m <twint1m.contact@gmail.com>
2023-05-09 13:02:16 +0300

    test

--- hard.js
+++ hard.js
@@ -1,9 +1,10 @@
-let num = 266219;
-let digits = num.toString().split('');
-let product = 1;
-for (let i = 0; i < digits.length; i++) {
-  product *= digits[i];
-}
-product = product * product * product;
-let resultString = product.toString();
-console.log(resultString.substring(0, 2));
+// let num = 266219;
+// let digits = num.toString().split('');
+// let product = 1;
+// for (let i = 0; i < digits.length; i++) {
+//   product *= digits[i];
+// }
+// product = product * product * product;
+// let resultString = product.toString();
+// console.log(resultString.substring(0, 2));
+console.log('test');
